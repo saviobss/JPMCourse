5eabbe2f842f5796241ce632304263000133b488
Sávio Evaristo <saviobss@gmail.com>
2022-08-11 16:01:49 -0300

    adiciona mais exercicios

--- 5/src/Main.java
+++ 5/src/Main.java
@@ -117,6 +117,23 @@ public class Main {
         //test 26
         DiagonalStar.printSquareStar(5);
         DiagonalStar.printSquareStar(8);
+
+        //test 27
+        //InputCalculator.inputThenPrintSumAndAverage();
+
+        //test 28
+        System.out.println(PaintJob.getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(PaintJob.getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(PaintJob.getBucketCount(2.75, 3.25, 2.5, 1));
+
+        System.out.println(PaintJob.getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(PaintJob.getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(PaintJob.getBucketCount(7.25, 4.3, 2.35));
+
+        System.out.println(PaintJob.getBucketCount(3.4,  1.5));
+        System.out.println(PaintJob.getBucketCount(6.26,  2.2));
+        System.out.println(PaintJob.getBucketCount(3.26,  0.75));
+
     }
 
 }
